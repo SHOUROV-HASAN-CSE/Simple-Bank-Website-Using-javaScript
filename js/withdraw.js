aa1cd9b2b75385b449329d54b28d86d5c4bb4b7c
Shourov Hasan <shourovhasancse@gmail.com>
2022-08-17 04:09:57 +0600

    done

--- js/withdraw.js
+++ js/withdraw.js
@@ -4,23 +4,37 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
   const withdrawField = document.getElementById('withdraw-field');
   const newWithdrawAmountString = withdrawField.value;
   const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+   // step 7: clear the deposit field
+   withdrawField.value = '';
+
+  if(isNaN(newWithdrawAmount)){
+    alert('Please Provide a valid Number');
+    return;
+  }
  
   // step 3: get the  deposit total Amount
   const withdrawTotalElement = document.getElementById('withdraw-total');
   const previousWithdrawTotalString = withdrawTotalElement.innerText;
   const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-  // step 4: add number to set the total deposit
-  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
-   // step 4: get balance current total
+ 
+   // step 5: get balance current total
    const balanceTotalElement = document.getElementById('balance-total');
   const previousBalanceTotalString = balanceTotalElement.innerText;
   const previousBalanceTotal = parseFloat(previousBalanceTotalString);
-  // step 5:calculate current total balance
+
+  if(newWithdrawAmount > previousBalanceTotal){
+    alert('তোমার বাপের ব্যাংক এ এতো টাকা নাই মনু !!!');
+    return;
+  }
+   // step 4: add number to set the total deposit
+   const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+   withdrawTotalElement.innerText = currentWithdrawTotal;
+
+  // step 6:calculate current total balance
   const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
   balanceTotalElement.innerText = newBalanceTotal;
 
-  // step 6: clear the deposit field
-  withdrawField.value = '';
+
 
 })
